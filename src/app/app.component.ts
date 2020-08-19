506eb7b840471bb02648ea1851617b97ee39475d
Paulo <paulituvitu@hotmail.com>
2020-08-19 17:09:14 -0300

    Iniciando pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  public message: string = "Hello World";
-}
+export class AppComponent {}
